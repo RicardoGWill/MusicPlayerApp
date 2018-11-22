bd45e30568be041a9c12c33156c2cc5e01d75673
ricardogwill <ricardogwill@gmail.com>
2018-11-22 00:03:21 -0500

    Added onDestroy().

--- app/src/main/java/com/ricardogwill/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/ricardogwill/musicplayerapp/MainActivity.java
@@ -170,4 +170,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         };
         thread.start();
     }
+
+    @Override
+    protected void onDestroy() {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        thread.interrupt();
+        thread = null;
+        super.onDestroy();
+    }
 }
